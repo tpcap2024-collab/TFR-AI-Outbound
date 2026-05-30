c6c7a4c74adbdcb68a05a5bda7f8b85b40029700
TFR AI <tpcap2024@gmail.com>
2026-05-30 09:07:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,8 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "TFR AI Running"
-
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.json
 
     return {
-        "status": "ok",
-        "received": data,
-        "fill_rate": 78
+        "status": "received",
+        "data": data
     }
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
