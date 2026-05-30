38730d463411d8f8edbd72ac6d5be0052319d344
TFR AI <tpcap2024@gmail.com>
2026-05-30 15:29:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,79 @@
+from flask import Flask, request, jsonify
+import cv2
+import numpy as np
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "TFR AI Running"
+
 @app.route("/predict", methods=["POST"])
 def predict():
 
     try:
 
-        print("HEADERS:", request.headers)
-
+        print("HEADERS:", dict(request.headers))
         print("RAW DATA:", request.data)
 
-        print("JSON:", request.get_json(silent=True))
+        data = request.get_json(silent=True)
+
+        print("JSON:", data)
+
+        if not data:
+            return jsonify({
+                "status": "error",
+                "message": "No JSON received"
+            }), 400
+
+        image_url = data.get("image_url")
+
+        if not image_url:
+            return jsonify({
+                "status": "error",
+                "message": "image_url not found",
+                "received": data
+            }), 400
+
+        response = requests.get(
+            image_url,
+            timeout=30
+        )
+
+        img_array = np.asarray(
+            bytearray(response.content),
+            dtype=np.uint8
+        )
+
+        img = cv2.imdecode(
+            img_array,
+            cv2.IMREAD_COLOR
+        )
+
+        if img is None:
+            return jsonify({
+                "status": "error",
+                "message": "cannot read image"
+            }), 400
+
+        h, w = img.shape[:2]
 
         return jsonify({
-            "status":"received"
+            "status": "success",
+            "width": int(w),
+            "height": int(h)
         })
 
     except Exception as e:
 
         return jsonify({
-            "error": str(e)
+            "status": "error",
+            "message": str(e)
         }), 500
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=10000
+    )
