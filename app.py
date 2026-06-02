6602033a25bb26bdcf313fe6281a6aa04b60cfbf
TFR AI <tpcap2024@gmail.com>
2026-06-02 14:59:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,58 +1,11 @@
-from flask import Flask, request, jsonify
-import traceback
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "TFR AI Running"
-
 @app.route("/predict", methods=["POST"])
 def predict():
 
-    try:
+    data = request.get_json()
 
-        print("=" * 80)
+    print("="*80)
+    print(data)
 
-        data = request.get_json(silent=True)
-
-        print("JSON:", data)
-
-        if not data:
-            return jsonify({
-                "status": "error",
-                "message": "No JSON"
-            }), 400
-
-        photo = data.get("photo")
-        datetime_value = data.get("datetime")
-
-        print("PHOTO:", photo)
-        print("DATETIME:", datetime_value)
-
-        if not photo:
-            return jsonify({
-                "status": "error",
-                "message": "photo empty"
-            }), 400
-
-        return jsonify({
-            "status": "success",
-            "photo": photo,
-            "datetime": datetime_value
-        })
-
-    except Exception as e:
-
-        print(traceback.format_exc())
-
-        return jsonify({
-            "status": "error",
-            "message": str(e)
-        }), 500
-
-if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=10000
-    )
+    return jsonify({
+        "status":"success"
+    })
